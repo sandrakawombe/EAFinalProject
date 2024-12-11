5048ecaa2012f99a621cab01960416ab28165b35
Sandra Kawombe <sandrakawombe@gmail.com>
2024-12-10 21:40:16 -0600

    Initial commit: Set up project structure and entities

---
+++ src/main/java/snakayima/miu/edu/finalprojectea/FinalProjectEaApplication.java
@@ -0,0 +1,13 @@
+package snakayima.miu.edu.finalprojectea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectEaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectEaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/snakayima/miu/edu/finalprojectea/FinalProjectEaApplicationTests.java
@@ -0,0 +1,13 @@
+package snakayima.miu.edu.finalprojectea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectEaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
